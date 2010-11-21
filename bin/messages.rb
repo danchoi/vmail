1b024d57e912add920f3da4a89e68ca20a73469a
Daniel Choi <dhchoi@gmail.com>
2010-11-21 14:44:21 -0500

    mysql rpad()

--- bin/messages.rb
+++ bin/messages.rb
@@ -7,7 +7,7 @@ end
 arg = quote_string(ARGV.first)
 
 sql = <<-END
-select uid, sender, subject from messages 
+select uid, RPAD(sender, 30, " "), subject from messages 
 inner join mailboxes_messages mm on messages.id = mm.message_id
 inner join mailboxes on mm.mailbox_id = mailboxes.id
 where mailboxes.label = "#{arg}"
