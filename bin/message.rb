2814e760a66c1d18f805040a7f5ff64083e8af8d
Daniel Choi <dhchoi@gmail.com>
2010-11-22 19:44:05 -0500

    minor

--- bin/message.rb
+++ bin/message.rb
@@ -8,10 +8,3 @@ cmd = "mysql -uroot gmail_development -e '#{sql} \\G'"
 res = `#{cmd}`
 
 puts res.split("\n")[1..-1].join("\n").sub(/text:\s/, '')
-exit
-if res.split("\n").size > 1 
-  res = res.split("\n")[1..-1].join("\n") # remove mysql header line
-  puts res.gsub('\\n', "\n")
-  
-end
-
