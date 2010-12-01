decbf7d2da0562c803cea04eab8a9521e66fb578
Daniel Choi <dhchoi@gmail.com>
2010-12-01 10:01:11 -0500

    removing activerecord-based code

--- bin/mailboxes.rb
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env ruby
-
-cmd = "mysql -uroot gmail -e 'select label from mailboxes order by position asc'"
-res = `#{cmd}`
-res = res.split("\n")[1..-1].join("\n") # remove mysql header line
-puts res
--- lib/message_ref.rb
+++
@@ -1,4 +0,0 @@
-class MessageRef < ActiveRecord::Base
-  validates_uniqueness_of :uid, :scope => :mailbox
-  belongs_to :message
-end
