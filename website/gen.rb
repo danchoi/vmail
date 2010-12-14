87171e8e5b0d6d9a61ddf4eaf8397f1a5208d045
Daniel Choi <dhchoi@gmail.com>
2010-12-13 23:11:32 -0500

    sharethis buttons

--- website/gen.rb
+++ website/gen.rb
@@ -3,5 +3,5 @@ require 'rdiscount'
 markdown = RDiscount.new File.read("page.markdown")
 
 template = File.read("vmail-template.html")
-out = Liquid::Template.parse(template).render 'content' => markdown.to_html
+out = Liquid::Template.parse(template).render 'content' => markdown.to_html, 'timestamp' => Time.now.to_i
 puts out
--- website/page.markdown
+++ website/page.markdown
@@ -3,3 +3,5 @@
 Vmail is a Vim interface to Gmail. Here is a screenshot:
 
 <img src="images-vmail/1.png"/>
+
+
