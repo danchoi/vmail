cf6106738580010571f61b40d653ec7eb5f39d2d
Daniel Choi <dhchoi@gmail.com>
2011-08-10 10:40:05 -0400

    Bump

--- lib/vmail/database.rb
+++ lib/vmail/database.rb
@@ -1,8 +1,16 @@
 require 'sequel'
 
+CREATE_TABLE_SCRIPT = File.expand_path("../../../db/create.sql", __FILE__)
+
 if !File.size?('vmail.db')
-  create_table_script = File.expand_path("../../../db/create.sql", __FILE__)
-  puts `sqlite3 vmail.db < #{create_table_script}`
+  puts `sqlite3 vmail.db < #{CREATE_TABLE_SCRIPT}`
+end
+
+db = Sequel.connect 'sqlite://vmail.db'
+if (r = db[:version].first) && r[:vmail_version] != Vmail::VERSION
+  puts "Vmail database version is outdated. Recreating."
+  `rm vmail.db`
+  puts `sqlite3 vmail.db < #{CREATE_TABLE_SCRIPT}`
 end
 
 DB = Sequel.connect 'sqlite://vmail.db'
@@ -10,6 +18,20 @@ DB = Sequel.connect 'sqlite://vmail.db'
 if DB[:version].count == 0
   DB[:version].insert(:vmail_version => Vmail::VERSION)
 end
+
+module Vmail
+  class Message < Sequel::Model(:messages)
+    set_primary_key :message_id
+    one_to_many :labelings
+    many_to_many :labels, :join_table => 'labelings'
+  end
+
+end
+
+if DB[:version].count == 0
+  DB[:version].insert(:vmail_version => Vmail::VERSION)
+end
+
 
 module Vmail
   class Message < Sequel::Model(:messages)
